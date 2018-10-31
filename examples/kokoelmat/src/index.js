c6434545c707e96c643860c073bd2f3913de3ac0
Joni Suominen <joni.suominen98@gmail.com>
2018-10-31 23:45:45 +0200

    save to remote repo

--- examples/kokoelmat/src/index.js
+++ examples/kokoelmat/src/index.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App'
+import axios from 'axios'
+
+axios
+    .get('http://localhost:3001/notes').
+    then(response => {
+    const notes = response.data
+    console.log(notes)
+})
 
 const notes = [
     {
