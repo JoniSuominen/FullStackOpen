a97ee1c20053e87417815b4ab53e6ef220e7cccb
Joni Suominen <joni.suominen98@gmail.com>
2019-02-23 06:38:24 +0200

    commit local work

---
+++ examples/reduxnotes/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import noteReducer from './noteReducer'
+import './App.css';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+      </div>
+    );
+  }
+}
+
+
+
+export default App;
---
+++ examples/reduxpractice/src/App.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom'
+import './App.css';
+import { createStore } from 'redux'
+
+const counterReducer = (state = 0, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1
+    case 'DECREMENT':
+      return state - 1
+    case 'ZERO':
+      return 0
+  }
+  return state
+}
+const store = createStore(counterReducer)
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <div>
+          {store.getState()}
+      </div>
+      <button onClick={e => store.dispatch({type: 'INCREMENT'})}>
+        plus
+      </button>
+      <button onClick={e => store.dispatch({type: 'DECREMENT'})}>
+        minus
+      </button>
+      <button onClick={e => store.dispatch({ type: 'ZERO' })}>
+        zero
+      </button>
+      </div>
+    )
+  }
+}
+
+const renderApp = () => {
+  ReactDOM.render(<App />, document.getElementById('root'))
+}
+
+renderApp()
+store.subscribe(renderApp)
+
+export default App;
